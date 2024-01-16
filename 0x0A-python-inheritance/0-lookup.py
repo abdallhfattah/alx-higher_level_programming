58800ab36427c939a88af9b090aece6d4ac348ed
abdallhfattah <abdallh.fattah@33gmail.com>
2024-01-16 11:20:51 +0200

    0x01-python-if_else_loops_functions/

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Defines a function that returns object attributes."""
-
-
-def lookup(obj):
-    """return a list of available attributes for the obj"""
-    return (dir(obj))
--- 0x0A-python-inheritance/4-inherits_from.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Defines a function that checks if an object is instance of a class."""
-
-
-def inherits_from(obj, a_class):
-    """returns whether obj is instance of a_class"""
-    return (issubclass(type(obj), a_class) and type(obj) != a_class)
