4a32e953ea865c3239fea99c405c77dbc9071d77
abdallhfattah <abdallh.fattah@33gmail.com>
2024-02-06 13:17:18 +0200

    square enchanced

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,16 +5,10 @@
 class Square:
     """Defines square"""
 
-    def __init__(self, size=0):
+    def __init__(self, size):
         """Constructor.
 
         Args:
             size: length of a square.
         """
-        if (size >= 0):
-            self.__size = size
-        else:
-            raise ValueError("size must be >= 0")
-
-    def area(self):
-        return self.__size**2
+        self.__size = size
