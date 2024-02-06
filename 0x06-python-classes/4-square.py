17daa1e86e9e5dbb65717e70663a6de39258d544
abdallhfattah <abdallh.fattah@33gmail.com>
2024-02-06 13:44:19 +0200

    almost RULE OF 5

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,16 +18,16 @@ class Square:
         self.__size = size
 
     def area(self):
-        return self.__size**2
-    
+        return self.__size ** 2
+
     @property
     def size(self):
         return self.__size
-    
-    @property.setter
-    def size(self , size):
+
+    @size.setter
+    def size(self, size):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size     
+        self.__size = size
