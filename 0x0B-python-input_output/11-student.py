94dc67d60150a79802f192cfa180a3e9e8013e6e
fusion <abdallh.fattah@33gmail.com>
2024-03-03 19:37:31 +0200

    [11] task

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,33 +6,20 @@ class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """Initialize a new Student."""
 
-        Args:
-            first_name: (str) first name of the student.
-            last_name: (str) last name of the student.
-            age: (int) age of the student.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student
-
-            Args:
-                attrs: (list) attributes to return
-        """
+        """retrieves a dictionary representation of a Student"""
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
         return self.__dict__
 
     def reload_from_json(self, json):
-        """Replace all attributes of the Student.
-
-        Args:
-            json (dict): The key/value pairs to replace attributes with.
-        """
+        """Replace all attributes of the Student."""
         for k, v in json.items():
             setattr(self, k, v)
