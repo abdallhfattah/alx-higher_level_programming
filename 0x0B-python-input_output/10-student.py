30d642a7fb7077c8031d59aeb416422459eb6324
fusion <abdallh.fattah@33gmail.com>
2024-03-03 19:28:11 +0200

    student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,26 +4,14 @@
 
 class Student:
     """Represent a student."""
-
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name: (str) first name of the student.
-            last_name: (str) last name of the student.
-            age: (int) age of the student.
-        """
+        """Initialize a new Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student
-
-            Args:
-                attrs: (list) attributes to return
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+        """retrieves a dictionary representation of a Student"""
+        if all(isinstance(ele ,str ) for ele in attrs):
+            return {a: getattr(self , a) for a in attrs if hasattr(self , a)}
         return self.__dict__
