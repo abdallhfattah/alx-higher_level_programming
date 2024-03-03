f99a78cf7d4a59b677d1d6d482e3df5b23b20783
fusion <abdallh.fattah@33gmail.com>
2024-03-03 19:32:50 +0200

    [10] task

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,6 +4,7 @@
 
 class Student:
     """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student."""
         self.first_name = first_name
@@ -12,6 +13,19 @@ class Student:
 
     def to_json(self, attrs=None):
         """retrieves a dictionary representation of a Student"""
-        if isinstance(attrs , list ) and all(isinstance(ele ,str ) for ele in attrs):
-            return {a: getattr(self , a) for a in attrs if hasattr(self , a)}
-        return self.__dict__
+        # if isinstance(attrs , list ) and all(isinstance(ele ,str ) for ele in attrs):
+        #     return {a: getattr(self , a) for a in attrs if hasattr(self , a)}
+        # return self.__dict__
+        try:
+            for attribute in attrs:
+                if not isinstance(attribute, str):
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+
+        my_dict = dict()
+        for key, val in self:
+            if key in attrs:
+                my_dict[key] = val
+
+        return my_dict
