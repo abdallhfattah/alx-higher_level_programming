12b31f4d5ffec3c140b2b67964f78e1843c5aae4
fusion <abdallh.fattah@33gmail.com>
2024-03-03 20:33:36 +0200

    [101] status for logs

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from sys import stdin
+
+status_codes = {
+    '200' : 0,
+    '301' : 0,
+    '400' : 0,
+    '401' : 0,
+    '402' : 0,
+    '403' : 0,
+    '404' : 0,
+    '500' : 0,
+}
+
+total_size = i = 0
+
+def log_printer():
+    """this function printing informations"""
+    print(f'File size: {total_size}')
+    for code , count in sorted(status_codes.items()):
+        if count > 0:
+            print(f"{code}: {count}") 
+
+try:
+    for line in stdin:
+        spplited_line = line.split()
+        status_code = spplited_line[-2]
+        total_size += spplited_line[-1]
+        status_codes[status_code] += 1
+        i += 1
+        if i %10 == 0:
+            log_printer()
+        log_printer()
+except KeyboardInterrupt as error:
+    log_printer()
