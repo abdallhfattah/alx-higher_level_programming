651f9e58fd53c7793d9d8e0499e723cfde73bd6a
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-01 16:23:04 +0300

    adding

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+function add (a, b) {
+  if (a === undefined || b === undefined) {
+    return undefined;
+  } else {
+    return parseInt(a) + parseInt(b);
+  }
+}
+console.log(add(process.argv[2], process.argv[3]));
