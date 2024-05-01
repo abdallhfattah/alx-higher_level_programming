f6f40cfc1960dce550c7b232c8a77672a1994ad9
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-01 15:47:48 +0300

    agruments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length > 2) {
-  console.log('Argument found');
+if (process.argv.length >= 2) {
+  console.log('Arguments found');
+} else if (process.argv.length === 1) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
