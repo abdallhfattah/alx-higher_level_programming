5b24c3ad6630cdf7fd125fc37707015380d32fad
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-01 16:29:12 +0300

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+if (isNaN(parseInt(process.argv[2]))) {
+    console.log(1);
+} else {
+    let result = 1;
+    for (let fact = 1; fact <= parseInt(process.argv[2]); fact++) {
+        result *= fact;
+    }
+    console.log(result);
+}
