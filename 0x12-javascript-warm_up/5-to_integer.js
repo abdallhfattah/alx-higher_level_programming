a08a81cea0b6a5cb2fbefab186117a68ddf5f520
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-01 16:10:06 +0300

    to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+function isNumber (value) {
+  return parseInt(value);
+}
+
+if (isNaN(isNumber(process.argv[2]))) {
+  console.log('Not a number');
+} else {
+  console.log('My number : ', parseInt(process.argv[2]));
+}
