eb1d0f8673cb342629f9dced9c9d07098605339f
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-01 17:43:05 +0300

    reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const result = new Array();
+  for (let i = list.length; i >= 0; i--) {
+    const element = list[i];
+    result.push(element);
+  }
+  return result;
+};
