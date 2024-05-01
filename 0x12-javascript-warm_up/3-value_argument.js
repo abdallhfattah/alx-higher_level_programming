f513136f086c94e9d760c5347aa8d4b233e85747
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-01 15:52:16 +0300

    value arguments

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-
-if (process.argv.length > 2) {
-  console.log('Arguments found');
-} else if (process.argv.length === 1) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
