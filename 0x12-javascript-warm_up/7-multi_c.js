63ef93813efd5d084e0942eb35931e4ce3131868
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-01 16:15:27 +0300

    multi c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+if (process.argv[2] === undefined || isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let index = 0; index < parseInt(process.argv[2]); index++) {
+    console.log('C is fun');
+  }
+}
