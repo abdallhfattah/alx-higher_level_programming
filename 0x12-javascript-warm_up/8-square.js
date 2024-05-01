932728d9447d335c8d5b0290eea4457f407f8eb5
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-01 16:20:39 +0300

    squares

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+if (process.argv[2] === undefined || isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing size');
+} else {
+  for (let index = 0; index < parseInt(process.argv[2]); index++) {
+    for (let i = 0; i < parseInt(process.argv[2]); i++) {
+      process.stdout.write('X');
+    }
+    console.log();
+  }
+}
