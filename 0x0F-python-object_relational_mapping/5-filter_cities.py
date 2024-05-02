bd9e15c4939a27168b08cca5efbc1b7b97244665
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-02 09:16:37 +0300

    filter states SQL injection free

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import sys
+
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    match = sys.argv[4]  # state
+    cur.execute(
+        """SELECT cities.name FROM cities JOIN states ON state.id = cities.states_id 
+        WHERE states.name LIKE %s""", (match, ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
