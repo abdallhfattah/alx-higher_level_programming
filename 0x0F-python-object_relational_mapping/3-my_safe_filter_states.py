9dad48d5c1903426b36772e3af229c2cc99b4411
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-02 08:32:27 +0300

    SQL injection stamina

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""  list target state from the database hbtn_0e_0_usa & safe from sql injections """
+import sys
+
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = conn.cursor()
+    state = sys.argv[4]
+    if (state.find('--') or state.find('TRUNCATE') or state.find('DROP') or state.find('ALTER')):
+        sys.exit()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(state))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
