8902a9b221fb1b3736e89e46381dcc355d0ecb4a
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-02 07:49:36 +0300

    starting with

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa statring with N"""
+import sys
+
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        if(row.startswith('N')):
+            print(row)
+    cur.close()
+    conn.close()
