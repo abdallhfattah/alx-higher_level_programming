e94490ffdf6200474234103a10b2825f11292342
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-02 08:45:57 +0300

    SQL injection stamina

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""  list target state from the database hbtn_0e_0_usa & safe from sql injections """
+"""  lists all states from the database hbtn_0e_0_usa """
 import sys
 
 import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = conn.cursor()
-    state = sys.argv[4]
-    if (state.find('--') or state.find('TRUNCATE') or state.find('DROP') or state.find('ALTER')):
-        cur.close()
-        conn.close()
-        sys.exit()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(state))
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
-    conn.close()
+    db.close()
