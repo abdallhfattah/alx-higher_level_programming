7e756c9ba5529c6ad82d7c40d97fd6dd967151f2
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-02 08:18:40 +0300

    filter state by name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = conn.cursor()
-    state = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name = '{}'".format(state))
+    cur.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
