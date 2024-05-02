8b25d4a2b3a75fd79e159a0bae661016acdfcc4b
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-02 08:14:02 +0300

    filter state by name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""  lists target states from the database hbtn_0e_0_usa """
+import sys
+
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = conn.cursor()
+    state = sys.argv[4]
+    cur.execute(f"SELECT * FROM states WHERE name = {state}")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
