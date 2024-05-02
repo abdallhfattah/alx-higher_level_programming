d665fc460fcae2d950c0668856e9f002f0efa425
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-02 08:06:59 +0300

    filtering throw sql query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,11 +8,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states WHERE name Like binary '%N' Order by states.id")
     rows = cur.fetchall()
     for row in rows:
-        id , state = row.split(',')
-        if(state.startswith(' (N')):
-            print(row)
+        print(row)
     cur.close()
     conn.close()
